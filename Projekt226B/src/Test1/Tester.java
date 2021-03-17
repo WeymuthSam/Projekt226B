62e8a3a9a912349860c6eb4cf051d1a5f56432dc
WeymuthSam <79984543+WeymuthSam@users.noreply.github.com>
2021-03-17 13:26:44 +0100

    WIe gohts dir?
    hoi

--- Projekt226B/src/Test1/Tester.java
+++ Projekt226B/src/Test1/Tester.java
@@ -4,7 +4,7 @@ public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Moin Meista. Wie gohts denn hüt?");
+		System.out.println("Mir gohts guet dir?");
 	}
 
 }
