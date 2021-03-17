964de911bfb94efafdf50d63c4f18c6f1e0608ed
Gadifish <79984733+Gadifish@users.noreply.github.com>
2021-03-17 13:30:11 +0100

    Antwort
    .

--- Projekt226B/src/Test1/Tester.java
+++ Projekt226B/src/Test1/Tester.java
@@ -4,7 +4,7 @@ public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Mir gohts guet dir?");
+		System.out.println("bestens");
 	}
 
 }
