87daf778277a03dbb53e91c30ac0851edaee8fd1
Gadifish <79984733+Gadifish@users.noreply.github.com>
2021-03-18 16:57:56 +0100

    Gui Test

--- Projekt226B/src/GUI/Checkout.java
+++ Projekt226B/src/GUI/Checkout.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class Checkout extends JPanel {
 
@@ -8,7 +9,11 @@ public class Checkout extends JPanel {
 	 * Create the panel.
 	 */
 	public Checkout() {
+		setLayout(null);
+		
+		JButton btnTest = new JButton("Test");
+		btnTest.setBounds(309, 34, 89, 23);
+		add(btnTest);
 
 	}
-
 }
